d162c8db4c7bff0ac18ef223369216bf567e14b6
Marcel Podlecki <251204@edu.p.lodz.pl>
2026-05-07 15:26:18 +0200

    feat: implementacja wirtualnego joysticka 2D i panelu akcji closes #26

--- app/src/main/java/com/example/watir_iot_app/feature/joystick/JoystickScreen.kt
+++ app/src/main/java/com/example/watir_iot_app/feature/joystick/JoystickScreen.kt
@@ -1,18 +1,37 @@
 package com.example.watir_iot_app.feature.joystick
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.watir_iot_app.feature.joystick.components.VirtualJoystick
 
 @Composable
 fun JoystickScreen() {
-    Box(
+    Column(
         modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Joystick Screen - WATIR IoT")
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally){
+        VirtualJoystick()
+        Spacer(modifier = Modifier.height(48.dp))
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Button(onClick = {
+
+            }) {Text("Podlej") }
+
+            Button(onClick = {
+
+            }) {Text("Zapisz pozycję") }
+        }
     }
 }
---
+++ app/src/main/java/com/example/watir_iot_app/feature/joystick/components/VirtualJoystick.kt
@@ -0,0 +1,51 @@
+package com.example.watir_iot_app.feature.joystick.components
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable fun VirtualJoystick(){
+    var thumbOffset by remember { mutableStateOf(Offset.Zero) }
+    Canvas(modifier =
+        Modifier
+            .size(200.dp)
+            .pointerInput(Unit){
+                detectDragGestures(
+                    onDragEnd = { thumbOffset = Offset.Zero}
+                ){
+                    change, dragAmount->
+                    change.consume()
+                    val newOffset = thumbOffset + dragAmount
+                    val distance = newOffset.getDistance()
+                    val maxRadius = 100f
+                    if(distance <= maxRadius){
+                        thumbOffset = newOffset
+                    }
+                    else{
+                        thumbOffset = newOffset / distance * maxRadius
+                    }
+                }
+            }) {
+        drawCircle(
+            color = Color.LightGray,
+            radius = 100f
+        )
+        drawCircle(
+            color = Color.Blue,
+            radius = 40f,
+            center = center + thumbOffset
+        )
+    }
+}
